dee7bfd820d84b1b1eb9480b5bde915db99d83f5
Mehmet <tekmanmehmetyasar@protonmail.com>
2023-09-13 20:32:53 +0300

    Error handling is improved in scraper/GetRandomDefinition()

--- scraper/GetRandomDefinition.go
+++ scraper/GetRandomDefinition.go
@@ -1,6 +1,7 @@
 package scraper
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"github.com/PuerkitoBio/goquery"
@@ -12,8 +13,6 @@ type Definition struct{
 	Description string
 	Example string
 	AuthorDate string
-	Upvotes string
-	Downvotes string
 }
 
 func GetRandomDefinition() Definition{
@@ -21,13 +20,18 @@ func GetRandomDefinition() Definition{
 
 	res,err := http.Get("https://www.urbandictionary.com/random.php")
 	if err!=nil {
-		log.Fatal(err)
+
+		fmt.Println("An error appeared in the get request, you may have a connection problem.")
+		fmt.Println("If you don't, please open an issue on Github. ")
+		log.Fatalf("Error: %s",err)
 	}
 
 	defer res.Body.Close()
 
 	if res.StatusCode != 200 {
+
 		log.Fatalf("HTTP Error: %d %s",res.StatusCode,res.Status)
+
 	}
 
 	doc,err := goquery.NewDocumentFromReader(res.Body)
