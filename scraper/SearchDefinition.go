0abedf8997edb3ac85ebc4ec878cc69f9db1b016
Mehmet <tekmanmehmetyasar@protonmail.com>
2023-09-13 20:41:25 +0300

    Now SearchDefinition() returns a an empty slice when there's no definitions (When it returns a 404 code)

--- scraper/SearchDefinition.go
+++ scraper/SearchDefinition.go
@@ -7,7 +7,6 @@ import (
 )
 
 
-// TODO: This function has no handlers when there's no definition.
 
 func SearchDefinition(searchTerm string,numDefs int) []Definition {
 
@@ -20,22 +19,30 @@ func SearchDefinition(searchTerm string,numDefs int) []Definition {
 	res,err := http.Get(url)
 
 	if err != nil {
+		
 		log.Fatal(err)
 	}
 
 	defer res.Body.Close()
 
 	if res.StatusCode != 200 {
+
+		// If page not found, function returns an empty slice to show
+		// that there are no definitions.
+		if res.StatusCode == 404 {
+			return make([]Definition,0)
+		}
+
 		log.Fatalf("HTTP Error: %d %s",res.StatusCode,res.Status)
 	}
 
 	doc,err := goquery.NewDocumentFromReader(res.Body)
 
 	if err != nil {
+		
 		log.Fatal(err)
 	}
 
-	// TODO: There should be a check here if any definitions exist at all!!
 
 	defSlice := make([]Definition,numDefs)
 
